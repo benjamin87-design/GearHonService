cd3757550d6338f6e786bc216d7803b57920049f
benjamin87-design <54944108+benjamin87-design@users.noreply.github.com>
2023-12-18 14:35:38 +0800

    about binding

--- GearHonService/ViewModels/AboutViewModel.cs
+++ GearHonService/ViewModels/AboutViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,27 @@ namespace GearHonService.ViewModels
 {
 	public partial class AboutViewModel : BaseViewModel
 	{
+		[ObservableProperty]
+		private string userEmail;
+		[ObservableProperty]
+		private string appVersion;
+		[ObservableProperty]
+		private string totalMachines;
+		[ObservableProperty]
+		private string totalCustomers;
+		[ObservableProperty]
+		private string totalHours;
+		[ObservableProperty]
+		private string phoneNumber;
+		[ObservableProperty]
+		private string email;
+
 		public AboutViewModel()
 		{
-
+			UserEmail = Preferences.Get("email", "");
+			AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			PhoneNumber = "Phone" + " " + "86-150-2214-3391";
+			Email = "Email" + " " + "benjamin.fehr@praewema.de";
 		}
 	}
 }
